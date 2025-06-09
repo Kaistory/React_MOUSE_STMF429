d297b7ea022a64aef637bd59c086b65849cbe4bc
Kaistory <duongkhai222204pt@gmail.com>
2025-06-09 19:35:34 +0700

    new

--- version_0_3/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ version_0_3/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -3,6 +3,8 @@
 #include "usbd_hid.h"
 #include "stm32f4xx_hal.h"
 extern USBD_HandleTypeDef hUsbDeviceHS;
+int16_t curentX = 0;
+int16_t curentY = 0;
 // Mouse HID Report Structure
 typedef struct {
     uint8_t buttons;
@@ -10,6 +12,7 @@ typedef struct {
     int8_t y;
     int8_t wheel;
 } MouseHID_Report_t;
+
 void SendMouseHIDUSB(int16_t x, int16_t y)
 {
     MouseHID_Report_t mouseReport;
@@ -46,11 +49,17 @@ void Screen1View::handleClickEvent(const ClickEvent& evt)
         // Add touch point for animation
 
         // Send mouse HID event
-    	SendMouseHIDUSB(evt.getX(),evt.getY()); // Left click
+    	if(curentX != evt.getX() || curentY != evt.getY() )
+    		{
+    				SendMouseHIDUSB(evt.getX(),evt.getY()); // Left click
+    			curentX = evt.getX();
+    			curentY = evt.getY();
+    		}
     	HAL_GPIO_TogglePin(GPIOG, GPIO_PIN_13);
         invalidate();
     } else if(evt.getType() == ClickEvent::RELEASED) {
         // Send mouse release
+
 //        SendMouseHID(0, 0);
     }
 }
