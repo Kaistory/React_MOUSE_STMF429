0e29ce9de8edff8f72aff14a98acc6351804043a
Kaistory <duongkhai222204pt@gmail.com>
2025-07-02 14:14:03 +0700

    clean main

--- version_0/STM32CubeIDE/Debug/Application/User/generated/image_Black.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_Black.o: \
- C:/TouchGFXProjects/React_MOUSE_STMF429/version_0/TouchGFX/generated/images/src/image_Black.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- version_0/STM32CubeIDE/Debug/Application/User/gui/Model.d
+++
@@ -1,6 +0,0 @@
-Application/User/gui/Model.o: \
- C:/TouchGFXProjects/React_MOUSE_STMF429/version_0/TouchGFX/gui/src/model/Model.cpp \
- ../../TouchGFX/gui/include/gui/model/Model.hpp \
- ../../TouchGFX/gui/include/gui/model/ModelListener.hpp
-../../TouchGFX/gui/include/gui/model/Model.hpp:
-../../TouchGFX/gui/include/gui/model/ModelListener.hpp:
